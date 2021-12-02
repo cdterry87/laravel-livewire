6231d3cf9d77f08f5dbfe7d36dafb7d6f4b5d38b
Chase Terry <chase.terry87@gmail.com>
2021-12-01 19:57:04 -0600

    Added some data binding

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,10 +6,12 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {
+    public $name = 'World';
+    public $loud = false;
+    public $greeting = ['Hello'];
+
     public function render()
     {
-        return view('livewire.hello-world', [
-            'name' => 'Livewire',
-        ]);
+        return view('livewire.hello-world');
     }
 }
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,11 @@
 <div>
-    Hello {{ $name}}
+    <input type="text" wire:model="name">
+    <input type="checkbox" wire:model="loud">
+    <select wire:model="greeting" multiple>
+        <option value="Hello">Hello</option>
+        <option value="Goodbye">Goodbye</option>
+        <option value="Adios">Adios</option>
+        <option value="Aloha">Aloha</option>
+    </select>
+    {{ implode(', ', $greeting) }} {{ $name }} @if($loud) ! @endif
 </div>
