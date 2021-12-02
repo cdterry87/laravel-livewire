b58ed68faf5b866dd45784952456812b82fd93a7
Chase Terry <chase.terry87@gmail.com>
2021-12-01 19:33:55 -0600

    Created a basic livewire component and loaded some data into it

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Livewire Intro</title>
+    @livewireStyles
 </head>
 <body>
-    <main>
-        Hello World
-    </main>
+    @livewire('hello-world')
+
+    @livewireScripts
 </body>
 </html>
